2caa491dad0c8bb9a81505807d584a71de76daf8
lukaszmosionek <mosioneklukasz@gmail.com>
2025-07-15 11:15:41 +0200

    Initial Commit

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# rm -r backend
+# rm -r frontend
+
+# # Klonowanie repozytoriów
+# git clone https://github.com/user/vue3-frontend.git frontend
+# git clone https://github.com/user/laravel-api.git backend
+
+# Budowanie i uruchamianie kontenerów
+docker-compose up --build
